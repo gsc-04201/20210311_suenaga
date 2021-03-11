119539dce5e48c5eceba883a4ae8ef8629f7907f
gsc-04201 <75969709+gsc-04201@users.noreply.github.com>
2021-03-11 21:54:04 +0900

    first-commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <input type="text" maxlength="8" v-model="zipcode">
+      <button @click="addressBtn">住所自動入力</button>
+    <br />
+    Address：<p>{{addressData}}</p>
+  </form>
+</template>
+
+<script>
+import axios from "axios"
+export default {
+  data() {
+    return {
+      zipcode: "",
+      addressData: ""
+    }
+  },
+  async created() {
+    const item = await axios.get(`https://api.zipaddress.net/?zipcode=`);
+  },
+  methods: {
+    addressBtn() {
+      axios.get(item + this.zipcode).then((res) => {
+        this.addressData = res.data.results[0];
+      })
+    }
+  }
+};
+</script>
